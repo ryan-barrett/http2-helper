2d0dfedc6392607fb306f3b5c4db1c7c29cf9d64
Ryan Barrett <ryan.barrett@spookyhouse.org>
2020-12-28 18:30:59 -0800

    v2

---
+++ src/examples.ts
@@ -0,0 +1,113 @@
+import { Http2Factory, Http2Listener, Http2SessionListener, ServerBroadcast } from './index';
+
+// const options = {
+//   key: getKeySomehow(),
+//   cert: getCertSomehow()
+// };
+
+const server = Http2Factory.Create('test');
+server.connect(8000);
+
+/**
+ * examples of actions taken on ALL servers
+ */
+// Http2Factory.disconnect(); // disconnect all servers
+
+// setTimeout(() => {
+//   Http2Factory.writeAll('yeet yeet big papa');
+// }, 4000);
+
+/**
+ * ---------------------------------------------
+ */
+
+/**
+ * Early example - this method will execute when a connection is made
+ */
+
+class Example {
+  /**
+   * run this method when a stream comes in from a client to the specified server
+   *
+   * @param stream
+   * @param headers
+   */
+  @Http2Listener('test')
+  public thisWillBeAStreamHandler(stream, headers) {
+    stream.write('hello ');
+
+    for (let i = 5; i > 0; i--) {
+      stream.write('hello ');
+    }
+  };
+
+  /**
+   * broadcast the output of this method to the entire server
+   */
+  @ServerBroadcast('test')
+  public broadcastTheOutput() {
+    return 'a whole new wooooorld';
+  }
+
+  @ServerBroadcast('test')
+  public async broadcastTheOutputAsync() {
+    return 'a whole new wooooorld async';
+  }
+
+  anotherNonHttp2Method() {
+    console.log('things are happening');
+  }
+}
+
+/**
+ * another class that also listens to incoming streams and takes action on them. In this case it just logs our headers
+ */
+
+class Example2 {
+  @Http2Listener('test')
+  public anotherStreamHandler(stream, headers) {
+    console.log('headers', headers);
+  }
+
+  // send another message 5 seconds after
+  @Http2Listener('test')
+  public sendSomeMessage(stream, headers) {
+    setTimeout(() => {
+      stream.write('hello again');
+    }, 5000);
+  }
+
+  /**
+   * run this method when a session is established. can be used to take action on certain origins / metadata
+   *
+   * @param session
+   */
+  @Http2SessionListener('test')
+  public logStreamOrigins(session) {
+    // console.log(session.originSet);
+  }
+}
+
+const example = new Example();
+
+/**
+ * broadcast test
+ */
+setTimeout(() => {
+  example.broadcastTheOutput();
+  example.broadcastTheOutputAsync();
+}, 5000);
+
+const example2 = new Example2();
+
+/**
+ * this is important: we can now broadcast messages across all live connections enabling system level messaging
+ */
+setTimeout(() => {
+  server.writeAll('yeet yeet');
+}, 8000);
+
+/**
+ * normal methods work too
+ */
+example.anotherNonHttp2Method();
---
+++ src/http2.ts
@@ -0,0 +1,242 @@
+import http2            from 'http2';
+import { EventEmitter } from 'events';
+import { v4 as uuid }   from 'uuid';
+
+type StreamListener = (stream: http2.Http2Stream, headers: http2.ClientSessionRequestOptions) => void;
+
+/**
+ * creates and manages our collection of servers
+ */
+
+class Http2FactorySingleton {
+  emitter = new EventEmitter();
+  _servers: { [key: string]: Http2Server } = {};
+
+  // FaCtOrY PaTtErN
+  public Create(name: string, key?: string, cert?: string) {
+    const newServer = new Http2Server(name, this.emitter, key, cert);
+    this.AddServer(name, newServer);
+    return newServer;
+  }
+
+  public GetServer(serverName: string): Http2Server {
+    if (!this._servers[serverName]) {
+      throw new Error('no http2 server found with that name');
+    }
+    return this._servers[serverName];
+  }
+
+  private AddServer(name: string, server: Http2Server): void {
+    if (this._servers[name]) {
+      throw new Error('an http2 server with that name already exists');
+    }
+    this._servers[name] = server;
+  }
+
+  /**
+   * disconnect ALL servers
+   */
+  @Broadcast()
+  async disconnect(): Promise<void> {
+  }
+
+  /**
+   * send a message to ALL servers e.g. system wide broadcast
+   *
+   * @param args
+   */
+  @Broadcast()
+  async writeAll(...args) {
+  }
+}
+
+/**
+ * an individual server
+ */
+class Http2Server {
+  private readonly _name: string;
+  public emitter = new EventEmitter();
+  protected _server: http2.Http2Server;
+  protected _streamCache = {};
+
+  constructor(name: string, factoryEmitter: EventEmitter, key?: string, cert?: string) {
+    this._name = name;
+    this._server = http2.createServer();
+
+    /**
+     * we allow the factory to have a line of communication directly to all servers
+     */
+    factoryEmitter.on('broadcast', (methodName: string, ...args) => {
+      Reflect.get(this, methodName).apply(this, args);
+    });
+  }
+
+  get name(): string {
+    return this._name;
+  }
+
+  /**
+   * send a message over all active connections for this server e.g. server wide broadcast
+   */
+  public writeAll(...args) {
+    for (const stream in this._streamCache) {
+      this._streamCache[stream].write(...args);
+    }
+  }
+
+  /**
+   * if we need to emit directly over the server
+   *
+   * @param event
+   * @param args
+   */
+  public emit(event: string, ...args) {
+    this._server.emit(event, ...args);
+  }
+
+  /**
+   * if we need to listen directly over the server
+   *
+   * @param event
+   * @param listener
+   */
+  public on(event: string, listener: StreamListener) {
+    this._server.on(event, listener);
+  }
+
+  public connect(port: number) {
+    this.initStreamEmitter();
+    this._server.listen(port);
+  }
+
+  public disconnect() {
+    this._server.close();
+  }
+
+  private removeFromCache(streamId: number) {
+    if (this._streamCache[streamId]) {
+      delete this._streamCache[streamId];
+    }
+  }
+
+  /**
+   * prepares the initial OK response when someone connects and then emits internally. This prevents a free for all
+   * for the server listener
+   *
+   * @private
+   */
+  private initStreamEmitter() {
+    this._server.on('stream', (stream, headers) => {
+      /**
+       * we have a separate emitter for events inside our application. There has to be an order of operations:
+       * 1) we receive the incoming stream
+       * 2) we respond OK
+       * 3) we feed the stream to application internal emitters for them to do with what they like
+       *
+       * we also want to expose the server itself through an interface. You can then directly use the server emitter if
+       * needed.
+       */
+
+      /**
+       * we maintain a cache of stream references for later use
+       */
+      const id = uuid();
+      Reflect.set(stream, 'streamId', id);
+      this._streamCache[id] = stream;
+
+      stream.on('close', () => {
+        this.removeFromCache(id);
+      });
+
+      stream.respond({ ':status': 200, 'content-type': 'text/plain' });
+      this.emitter.emit(`${this.name}:Http2Stream`, stream, headers);
+    });
+
+    /**
+     * we also make the session available to the application interior once it is ready. We can use this for things
+     * such as seeing where the connection came from
+     */
+    this._server.on('session', (session) => {
+      this.emitter.emit(`${this.name}:Http2Session`, session);
+    });
+  }
+}
+
+/**
+ * export as a singleton
+ */
+export const Http2Factory = new Http2FactorySingleton();
+
+/**
+ * Decorators!
+ */
+
+/**
+ * create a stream listener for a given http2 server
+ *
+ * @constructor
+ * @param serverName
+ */
+export function Http2Listener(serverName: string) {
+  return function Http2Listener(target, propertyKey, descriptor) {
+    const server = Http2Factory.GetServer(serverName);
+    server.emitter.on(`${serverName}:Http2Stream`, function (stream: http2.Http2Stream, headers: http2.ClientSessionRequestOptions) {
+      descriptor.value(stream, headers);
+    });
+  };
+}
+
+/**
+ * create a session listener for a given http2 server
+ *
+ * @param serverName
+ * @constructor
+ */
+export function Http2SessionListener(serverName: string) {
+  return function Http2SessionListenerInner(target, propertyKey, descriptor) {
+    const server = Http2Factory.GetServer(serverName);
+    server.emitter.on(`${serverName}:Http2Session`, function (session: http2.Http2Session) {
+      descriptor.value(session);
+    });
+  };
+}
+
+/**
+ * broadcast the output of decorated method to all connections on the specified server
+ *
+ * @param serverName
+ * @constructor
+ */
+export function ServerBroadcast(serverName: string) {
+  return function ServerBroadcastInner(target, propertyKey, descriptor) {
+    const { value } = descriptor;
+
+    descriptor.value = function (...args) {
+      if (value[Symbol.toStringTag] === 'AsyncFunction') {
+        value(...args).then((res) => {
+          const server = Http2Factory.GetServer(serverName);
+          server.writeAll(res);
+        });
+      }
+      else {
+        const server = Http2Factory.GetServer(serverName);
+        server.writeAll(value(...args));
+      }
+    };
+  };
+}
+
+/**
+ * broadcast a method to all servers
+ *
+ * @constructor
+ */
+export function Broadcast() {
+  return function BroadcastInner(target, propertyKey, descriptor) {
+    const { value } = descriptor;
+    descriptor.value = function () {
+      value();
+      this.emitter.emit('broadcast', propertyKey, ...Object.values(arguments));
+    };
+  };
+};
